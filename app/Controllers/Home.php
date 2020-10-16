1d99e6131c7abc496d615221f5b6b749f2bf91a5
asbiqalalawi <asbiqalalawi@gmail.com>
2020-10-16 17:03:27 +0700

    pertemuan 3

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,10 +1,21 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		return view('welcome_message.php');
+	}
+
+	public function show()
+	{
+		$data['nama'] = 'Asbiq';
+		$data['jurusan'] = 'Ilmu Komputer';
+		echo view('mahasiswa/header');
+		echo view('mahasiswa/index', $data);
+		echo view('mahasiswa/footer');
 	}
 
 	//--------------------------------------------------------------------
