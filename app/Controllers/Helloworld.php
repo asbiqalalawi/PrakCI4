6b290a3f04f6dd5ae19b252e8cf3c7311dfbaa14
asbiqalalawi <48611998+asbiqalalawi@users.noreply.github.com>
2020-11-15 06:42:44 +0700

    Update Helloworld.php

--- app/Controllers/Helloworld.php
+++ app/Controllers/Helloworld.php
@@ -6,11 +6,8 @@ class Helloworld extends BaseController
 {
     public function index($nama, $npm)
     {
-<<<<<<< HEAD
         echo $nama . "<br>";
-=======
         echo $nama. "<br>";
->>>>>>> df3fea4917a300414f062e43420095d5216b10d6
         echo $npm;
     }
     public function show()
@@ -18,8 +15,5 @@ class Helloworld extends BaseController
         echo "Asbiq<br>";
         echo $this->name;
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> df3fea4917a300414f062e43420095d5216b10d6
+
